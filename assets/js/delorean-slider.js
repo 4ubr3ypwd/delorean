fb6cb43ab2a69014a0a6fda34e37083aa76894c3
Aubrey Portwood <aubreypwd@gmail.com>
2018-01-26 17:33:55 -0700

    Style the slider.

--- assets/js/delorean-slider.js
+++ assets/js/delorean-slider.js
@@ -12,29 +12,64 @@ if ( ! window.hasOwnProperty( 'deloreanSlider' ) ) {
 	 * Slider.
 	 */
 	window.deloreanSlider = ( function( $, pub ) {
+		var $slider       = 'undefined';
+		var currentCursor = 'undefined';
 
+		/**
+		 * Add a query argument to the URL.
+		 *
+		 * @author Aubrey Portwood
+		 * @since  1.0.0
+		 *
+		 * @param  {String} s     The URL.
+		 * @param  {String} param The query argument.
+		 * @param  {String} value The value for the argument.
+		 *
+		 * @return {String}       The URL with the value added.
+		 */
 		function addQueryArg( s, param, value ) {
 			return s += ( s.match( /[\?]/g ) ? '&' : '?' ) + param.toString() + '=' + value.toString(); // 'param=value';
 		}
 
-		function init() {
-			var $slider = jQuery( '#cursors' );
-			var $handle = $( '.ui-slider-handle', $slider );
-			var latest  = 'Latest';
+		/**
+		 * Get the Slider.
+		 *
+		 * @author Aubrey Portwood
+		 * @since  1.0.0
+		 *
+		 * @return {Object} jQuery object of the slider.
+		 */
+		function slider() {
+			if ( 'undefined' === $slider ) {
+				$slider  = $( '#cursors' );
+			}
 
-			if ( ! $slider.length ) {
+			return $slider;
+		}
+
+		/**
+		 * Setup the Delorean slider.
+		 *
+		 * @author Aubrey Portwood
+		 * @since  1.0.0
+		 *
+		 * @return {undefined} Bail if the slider isn't ready.
+		 */
+		function setupSlider() {
+			if ( ! slider().length ) {
+
+				// No slider?
 				return;
 			}
 
-			var cursors = $slider.data( 'cursors' );
-			var cursor  = $slider.data( 'cursor' ).toString();
-
-			var start = cursors.length;
-			if ( '0' !== cursor.toString() ) {
-				start = cursors.indexOf( cursor );
+			var cursors = slider().data( 'cursors' );
+			currentCursor  = slider().data( 'cursor' ).toString();
+			var start   = cursors.length;
+			if ( '0' !== currentCursor.toString() ) {
+				start = cursors.indexOf( currentCursor );
 			}
 
-			$slider.slider( {
+			slider().slider( {
 				range: false,
 				value: start,
 				min: 0,
@@ -46,25 +81,55 @@ if ( ! window.hasOwnProperty( 'deloreanSlider' ) ) {
 					} else {
 						window.location.href = addQueryArg( window.location.href, 'cursor', '0' );
 					}
-				},
-				create: function() {
-					if ( $handle.length && '0' !== cursor.toString() ) {
-						$handle.text( cursor );
-					} else {
-						$handle.text( latest );
-					}
-				},
-				slide: function( event, choice ) {
-					if ( $handle.length && cursors[ choice.value ] ) {
-						$handle.text( cursors[ choice.value ] );
-					} else {
-						$handle.text( latest );
-					}
 				}
 			} );
 		}
 
-		jQuery( document ).ready( init );
-		return pub; // Return public things.
+		/**
+		 * Adjust the wrapper.
+		 *
+		 * The Delorean wrapper spans 100% the width, but that can
+		 * end up under some of the help screen buttons.
+		 *
+		 * This adjusts them
+		 *
+		 * @author Aubrey Portwood
+		 * @since  1.0.0
+		 */
+		function adjustDelorean() {
+			var $wrapper = $( '#delorean' );
+			if ( ! $wrapper.length ) {
+
+				// No wrapper?
+				return;
+			}
+
+			var $helpButtons = $( '.screen-meta-toggle' );
+			if ( ! $helpButtons.length ) {
+
+				// None, leave it alone.
+				return;
+			}
+
+			// Reset the wrapper's width again.
+			$wrapper.css( { width: '95%' } );
+
+			// Figure out how wide all the buttons are.
+			var width = 0;
+			$helpButtons.each( function( i, v ) {
+				width = width + $( v ).width() + 5;
+			} );
+
+			// Make the wrapper 100% the width minus the screen helper buttons.
+			$wrapper.css( { width: $wrapper.width() - width } );
+		}
+
+		// When the DOM is ready.
+		$( document ).ready( setupSlider );
+		$( document ).ready( adjustDelorean );
+		$( window ).on( 'resize', adjustDelorean );
+
+		// Return public things.
+		return pub;
 	} ( jQuery, {} ) );
 } // End if().
--- includes/class-timeline.php
+++ includes/class-timeline.php
@@ -23,14 +23,21 @@ class Timeline {
 	 * @since  1.0.0
 	 */
 	public function hooks() {
-		add_filter( 'admin_footer_text', array( $this, 'timeline' ) );
-		add_action( 'admin_enqueue_scripts', array( $this, 'scripts' ) );
+		add_action( 'admin_notices', array( $this, 'timeline' ) );
+		add_action( 'admin_enqueue_scripts', array( $this, 'enqueue' ) );
 	}
 
-	public function scripts() {
+	/**
+	 * Scripts & Styles.
+	 *
+	 * @author Aubrey Portwood
+	 * @since  1.0.0s
+	 */
+	public function enqueue() {
 		wp_enqueue_script( 'jquery-ui-core' );
 		wp_enqueue_script( 'jquery-ui-slider' );
 		wp_enqueue_style( 'jquery-ui-theme-base', plugins_url( 'assets/css/jquery-ui.css', app()->plugin_file ), array(), app()->version() );
+		wp_enqueue_style( 'delorian-slider', plugins_url( 'assets/css/delorean-slider.css', app()->plugin_file ), array( 'jquery-ui-theme-base' ), app()->version() );
 		wp_enqueue_script( 'delorean-slider', plugins_url( 'assets/js/delorean-slider.js', app()->plugin_file ), array( 'jquery', 'jquery-ui-core', 'jquery-ui-slider' ), app()->version(), false );
 	}
 
@@ -39,57 +46,19 @@ class Timeline {
 	 *
 	 * @author Aubrey Portwood
 	 * @since  1.0.0
-	 *
-	 * @param string $default The default value for admin_footer_text filter.
-	 *
-	 * @return string The content.
 	 */
-	public function timeline( $default ) {
+	public function timeline() {
 		global $table_prefix;
 		global $cursor;
-		ob_start();
 		?>
 
-		<div id="cursors" data-cursor="<?php echo absint( $cursor ); ?>" data-cursors="<?php echo esc_attr( wp_json_encode( app()->cursors->get_cursors() ) ); ?>">
-			<div class="ui-slider-handle"></div>
+		<div id="delorean">
+			<div class="wp-slider" id="cursors" data-cursor="<?php echo absint( $cursor ); ?>" data-cursors="<?php echo esc_attr( wp_json_encode( app()->cursors->get_cursors() ) ); ?>">
+				<div class="ui-slider-handle"></div>
+			</div>
+			<span class="label">Delorean</span>
 		</div>
-		<span id="cursors-chosen"></span>
 
 		<?php
-		return ob_get_clean();
-	}
-
-	/**
-	 * Whether to use an <a> tag or a <span>.
-	 *
-	 * @author Aubrey Portwood
-	 * @since  1.0.0
-	 *
-	 * @param  string $cursor The cursor.
-	 * @return string         a or span tag.
-	 */
-	public function tag( $cursor ) {
-		if ( app()->cursors->is_cursor( $cursor ) ) {
-			return 'span';
-		}
-
-		return 'a';
-	}
-
-	/**
-	 * What link to use for a cursor.
-	 *
-	 * @author Aubrey Portwood
-	 * @since  1.0.0
-	 *
-	 * @param  string $cursor The cursor.
-	 * @return string         # to link to nothing, or the cursor invocation URI.
-	 */
-	public function link( $cursor ) {
-		if ( app()->cursors->is_cursor( $cursor ) ) {
-			return '#';
-		}
-
-		return esc_url( add_query_arg( 'cursor', $cursor, $_SERVER['REQUEST_URI'] ) );
 	}
 }
